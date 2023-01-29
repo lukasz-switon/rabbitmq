a6a2a8e6ad956cda7515c2a957ed50e2ee18a4c0
Łukasz Świtoń <lukas06.04.93@gmail.com>
2023-01-29 18:08:13 +0100

    - Removed auto generated Main classes - Added configuration to both modules with port number - Added Spring Boot main classes

---
+++ consumer/src/main/java/com/lukaszswiton/rabbitmq/consumer/ConsumerSpringBootApp.java
@@ -0,0 +1,11 @@
+package com.lukaszswiton.rabbitmq.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerSpringBootApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerSpringBootApp.class, args);
+    }
+}
---
+++ producer/src/main/java/com/lukaszswiton/rabbitmq/producer/ProducerSpringBootApp.java
@@ -0,0 +1,11 @@
+package com.lukaszswiton.rabbitmq.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerSpringBootApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerSpringBootApp.class, args);
+    }
+}
